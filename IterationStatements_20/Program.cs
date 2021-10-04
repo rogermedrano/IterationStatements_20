bc123bb4487192430ce80675af048e8a80b0fc8b
Roger Medrano <email@example.com>
2021-10-04 09:45:16 -0700

    Reiterating iterations

---
+++ IterationStatements_20/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace IterationStatements_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for loop
+            /* FOR: while declaring and initializine interger i to value of 0.
+            as long as i is less than 0, write to console the value of i and 
+            increment i by 1*/
+
+            Console.WriteLine("Iteration Statements");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 0; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i= 0; i < 0; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 0; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 0; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 0; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 0; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 0; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 0; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 0; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 0; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 0; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 0; i++)
+            {
+                Console.WriteLine($"i");
+            }
+            
+
+        }
+    }
+}
